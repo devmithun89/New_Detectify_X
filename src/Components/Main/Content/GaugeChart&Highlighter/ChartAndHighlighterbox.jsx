0af9241ac3cf68d8bd92138f623aa9737c6d556f
devmithun89 <mithungorai662@gmail.com>
2023-09-23 09:30:50 +0530

    added results screen some heading and fix % issue

--- src/Components/Main/Content/GaugeChart&Highlighter/ChartAndHighlighterbox.jsx
+++ src/Components/Main/Content/GaugeChart&Highlighter/ChartAndHighlighterbox.jsx
@@ -9,11 +9,15 @@ function ChartAndHighlighterbox({clear, score, matchTxt, value}) {
     <>
       <div className="max-w-xl mx-auto mt-10 rounded-md shadow ">
         <div class="max-w-xl mx-auto bg-white p-6  box-border  rounded-md shadow-lg">
-          <div className="flex justify-end pb-2">
+          <div className="flex items-center justify-between pb-2">
+            <p>
+              Probability of AI{" "}
+              <samp className="text-red-600">{Math.floor(score) / 1.0}%</samp>
+            </p>{" "}
             <button
               onClick={clear}
               className="py-3 px-8 font-Jost font-normal text-base text-white rounded-3xl bg-primary hover:bg-grey duration-500 hover:text-white border-lite.grey border-2 border-solid">
-              clear
+              Clear
             </button>
           </div>
           <div className="h-auto p-2 border rounded-md resize-y md:w-full font-Jost focus:outline-none focus:ring focus:border-blue-300">
@@ -21,8 +25,7 @@ function ChartAndHighlighterbox({clear, score, matchTxt, value}) {
               id="gauge-chart6"
               animate={true}
               nrOfLevels={15}
-              // style={{width:500}}
-              percent={Math.round(score) / 10}
+              percent={Math.floor(score) / 100}
               textColor={"red"}
               needleColor="#345243"
             />
--- src/Components/Main/Content/InputBox/Inputbox.jsx
+++ src/Components/Main/Content/InputBox/Inputbox.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function Inputbox({value, onChange, match, clear}) {
+function Inputbox({value, onChange}) {
   return (
     <div>
-        <div className='max-w-xl mx-auto mt-10 rounded-md shadow '>
-          <div class="max-w-xl mx-auto bg-white p-6  box-border  rounded-md shadow-lg">
-          <label for="message" class="block text-gray-700 font-Jost font-bold mb-2">Add some text:</label>
-          <textarea  value={value} onChange={(e) => onChange(e.target.value)} id="message" name="message" class="w-full h-48 p-2 border rounded-md font-Jost resize-y focus:outline-none focus:ring focus:border-blue-300" placeholder="Type your text here..."></textarea>
-      </div>    
+      <div className="max-w-xl mx-auto mt-10 rounded-md shadow ">
+        <div class="max-w-xl mx-auto bg-white p-6  box-border  rounded-md shadow-lg">
+          <label for="message" class="block text-gray-700 font-Jost font-bold mb-2">
+            Add some text:
+          </label>
+          <textarea
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            id="message"
+            name="message"
+            class="w-full h-48 p-2 border rounded-md font-Jost resize-y focus:outline-none focus:ring focus:border-blue-300"
+            placeholder="Type your text here..."></textarea>
+        </div>
+      </div>
     </div>
-
-
-    </div>
-  )
+  );
 }
 
-export default Inputbox
+export default Inputbox;
